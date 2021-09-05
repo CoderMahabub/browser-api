4e056dc64e71170c35dee416cb72f2ca3d066b6a
CoderMahabub <codermahabub@gmail.com>
2021-09-06 01:04:03 +0600

    Module Introduction and how internet works

---
+++ explore.js
@@ -0,0 +1,10 @@
+const name = 15;
+const student = {
+    name: 'jalil', friend: 'borsha', job: 'business',
+    makeMovie: function (name) {
+        console.log(name);
+    }
+}
+
+student.makeMovie('din raat');
+// document.getElementById('name');
